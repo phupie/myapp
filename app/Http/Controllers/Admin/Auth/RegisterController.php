9b6a040bd4345ac8a232a6f2518d77ba74f7298d
pupi <pupi.yh@gmail.com>
2021-09-27 07:43:51 +0000

    ver 1.5

--- app/Http/Controllers/Admin/Auth/RegisterController.php
+++ app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -77,7 +77,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return A::create([
+        return Admin::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
